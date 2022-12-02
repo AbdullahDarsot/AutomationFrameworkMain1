85524bd285a2339b4ea2f77006b975f1507e6b86
Abdullah Darsot <abdullahdarsot220@gmail.com>
2022-12-02 15:29:52 +0000

    Scenario 2,3,4 - Working

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -4,9 +4,12 @@ import static utils.selenium.Driver.browser;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import utils.selenium.Settings;
+import java.util.List;
 
 public class BasePage extends Page {
     private String getTitle() { return driver.getTitle(); }
+    private String getUrl() {return driver.getCurrentUrl(); }
+    private String getPageSource() {return driver.getPageSource(); }
 
     public WebDriver driver = browser();
 
@@ -21,4 +24,19 @@ public class BasePage extends Page {
         System.out.println(":: The title of the site is: " + getTitle());
     }
 
+    public void validatePageUrl(String expectedUrl) {
+        Assert.assertTrue(getUrl().contains(expectedUrl));
+        System.out.println(":: The page Url is: " + getUrl());
+    }
+    public void validatePageSource(String expectedPageSource) {
+        Assert.assertTrue(getPageSource().contains(expectedPageSource));
+        System.out.println(":: The page source is: " + getPageSource());
+    }
+
+    public void validateMultipleInPageSource(List<String> table) {
+        for (String row : table) {
+            Assert.assertTrue(getPageSource().contains(row));
+            System.out.println("The text " + row + " is in the PageSource");
+        }
+    }
 }
--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import pages.BasePage;
 import pages.Page;
+import java.util.List;
 
 public class BaseSteps extends Page {
 
@@ -16,5 +17,27 @@ public class BaseSteps extends Page {
     @Then("^they see the page title contains \"([^\"]*)\"$")
     public void they_see_the_page_title_contains(String expectedTitle) {
         instanceOf(BasePage.class).validatePageTitle(expectedTitle);
+    }
+
+    @Then("^they see the page Url contains \"([^\"]*)\"$")
+    public void theySeeThePageUrlContains(String expectedUrl) {
+        instanceOf(BasePage.class).validatePageUrl(expectedUrl);
+    }
+
+    @Then("^I see \"([^\"]*)\" in the PageSource$")
+    public void iSeeInThePageSource(String expectedText) {
+        String expectedPageSource = new String();
+        instanceOf(BasePage.class).validatePageSource(expectedPageSource);
+    }
+
+    @Then("they see {string} in the PageSource")
+    public void theySeeInThePageSource(String arg0) {
+        String expectedPageSource = new String();
+        instanceOf(BasePage.class).validatePageSource(expectedPageSource);
+    }
+
+    @Then("^they see$")
+    public void theySee(List<String> existsInPageSource) {
+        instanceOf(BasePage.class).validateMultipleInPageSource(existsInPageSource);
     }
 }
